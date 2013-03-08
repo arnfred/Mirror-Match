5dfda3d140cd3f7d52f78d72bd2bf167d2663859
Jonas Arnfred <jonas@ifany.org>
2013-03-08 10:27:02 +0000

    Modified plot so matches work

--- code/descriptors/plot.py
+++ code/descriptors/plot.py
@@ -33,13 +33,26 @@ def appendimages(im1, im2) :
 
 
 
-def features(im, locs) :
+def getPositions(keypoints) : 
+    pos = numpy.zeros([len(keypoints), 2])
+    for i in range(len(keypoints)) :
+        pos[i][0] = keypoints[i].pt[0]
+        pos[i][1] = keypoints[i].pt[1]
+    return pos
+
+
+
+def keypoints(im, locs) :
 	""" show image with features. input: im (image as array), 
 	    locs (row, col, scale, orientation of each feature) 
 	"""
+	# Extract positions from keypoints
+	pos = getPositions(locs)
+
+	# Plot all keypoints
 	pylab.gray()
 	pylab.imshow(im)
-	pylab.plot([p[1] for p in locs], [p[0] for p in locs], '.b')
+	pylab.plot([p[1] for p in pos], [p[0] for p in pos], '.b')
 	pylab.axis('off')
 	pylab.show()
 
@@ -47,17 +60,24 @@ def features(im, locs) :
 
 def matches(im1, im2, locs1, locs2, matchscores) :
 	""" show a figure with lines joining the accepted matches in im1 and im2
-		input: im1,im2 (images as arrays), locs1,locs2 (location of features), 
-		matchscores (as output from 'match'). """
+	    input: im1,im2 (images as arrays), locs1,locs2 (location of features), 
+	    matchscores (as output from 'match'). 
+	"""
+	# Extract positions from keypoints
+	pos1 = getPositions(locs1)
+	pos2 = getPositions(locs2)
 
+	# Construct unified image
 	im3 = appendimages(im1,im2)
 
+	# Display image
 	pylab.gray()
 	pylab.imshow(im3)
 
+	# Plot all lines
 	cols1 = im1.shape[1]
 	for i in range(len(matchscores)):
 		if matchscores[i] != None:
-			pylab.plot([locs1[i,1], locs2[int(matchscores[i]),1]+cols1], [locs1[i,0], locs2[int(matchscores[i]),0]], 'c')
+			pylab.plot([pos1[i,1], pos2[int(matchscores[i]),1]+cols1], [pos1[i,0], pos2[int(matchscores[i]),0]], 'c')
 	pylab.axis('off')
 	pylab.show()
