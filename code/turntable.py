c4fa3085d57683ccc3cd24c90cfdc115d6b3aff8
Jonas Arnfred <jonas@ifany.org>
2013-11-19 11:14:15 +0000

    Included evaluation function for turntable

--- code/turntable.py
+++ code/turntable.py
@@ -21,6 +21,7 @@ import matching
 import mirrormatch
 import colors
 import random
+import pylab
 
 ####################################
 #                                  #
@@ -28,14 +29,59 @@ import random
 #                                  #
 ####################################
 
-def get_turntable_path(image_set, object_type, angle, camera_position, turntable_dir = "../../turntable") :
+
+def evaluate(match_fun, angles, object_type, thresholds, params = {}) :
+    """ Returns number of correct and total matches of match_fun on object:
+        match_fun : Function (Function that takes a list of paths and returns matches)
+        angles : Int (Rotation in degrees. Must be divisible by 5)
+        object_type : String (the object pictured on the turntable)
+        thresholds : List[Float] (list of ratio thresholds)
+        params : Dict (Set of parameters for matching etc)
+    """
+
+    # Get distance_threshold
+    distance_threshold = params.get("distance_threshold", 5)
+    verbose = params.get("verbose", False)
+    
+    # Get paths to the three images
+    path_A = get_turntable_path(object_type, angles[0], "Bottom")
+    path_B = get_turntable_path(object_type, angles[0], "Top")
+    path_C = get_turntable_path(object_type, angles[1], "Bottom")
+    
+    # Get match results
+    matches = match_fun([path_A, path_C])(1.0)
+    
+    if verbose :
+        print("Found matches")
+    
+    # Get ground trouth
+    distances = match_distances(matches[0], angles, object_type, distance_threshold)
+    
+    if verbose :
+        print("Found distances")
+    
+    # Get number of matches given a threshold
+    def get_total(t) :
+        return int(sum([1 for d,u in zip(distances, matches[2]) if u <= t]))
+    
+    # Get number of correct matches given a threshold
+    def get_correct(t) : 
+        return int(sum([1 for d,u in zip(distances, matches[2]) if u <= t and d < distance_threshold]))
+    
+    # Collect precision per ratio_threshold
+    correct = [[get_correct(t)] for t in thresholds]
+    total = [[get_total(t)] for t in thresholds]
+    return correct, total
+
+
+def get_turntable_path(object_type, angle, camera_position, turntable_dir = "../../turntable") :
     """ Returns path to image of image_set and object_type where:
-        image_set : Int (the main folder)
         object_type : String (the object pictured on the turntable)
         angle : Int (Rotation in degrees. Must be divisible by 5)
         camera_position : String ("Top" or "Bottom")
         turntable_dir : String (path to folder with image sets)
     """
+    image_set = get_image_set(object_type)
     directory = "%s/imageset_%i/%s/%s" % (turntable_dir, image_set, object_type, camera_position)
     file_names = os.listdir(directory)
     file_glob = "img_1-%03i_*_0.JPG" % angle
@@ -44,14 +90,15 @@ def get_turntable_path(image_set, object_type, angle, camera_position, turntable
 
 
 
-def load_calibration_image(image_set, angle, camera_position, pattern_position, scale = None, turntable_dir = "../../turntable") :
+def load_calibration_image(object_type, angle, camera_position, pattern_position, scale = None, turntable_dir = "../../turntable") :
     """ Loads a checkerboard pattern image taken from the same angle and rotation:
-        image_set : Int (the image set folder)
         angle : Int (Rotation in degrees. Must be divisible by 5)
         pattern_position : String (either: "flat", "angled" or "steep")
         scale : Float (the calibration images are double size, so put 2.0)
         turntable_dir : String (path to folder with image sets)
     """
+    image_set = get_image_set(object_type)
+    directory = "%s/imageset_%i/%s/%s" % (turntable_dir, image_set, object_type, camera_position)
     pattern_index = { "flat" : 0, "steep" : 1, "angled" : 2 }[pattern_position]
     image_index = (angle / 5) * 3 + 1 + pattern_index
     image_path = "%s/imageset_%i/%s/%s/calib%i.jpg" % (turntable_dir, image_set, "Calibration", camera_position, image_index)
@@ -80,22 +127,25 @@ def get_calibration_points(img, pattern_position) :
 
 
 
-def get_foundamental_matrix(image_set, angles, camera_position, scale = 2.0, return_points = False) :
+def get_foundamental_matrix(object_type, angles, camera_position, scale = 2.0, return_points = False) :
     """ Returns fundamental matrix:
-        image_set : Int (the image set folder)
         angles : (Int, Int) (two angles in degrees. Must be divisible by 5)
         camera_position : String (either "Top" or "Bottom")
         scale : Float (scale of images compared to calibration images)
         return_points : Boolean (set to True if you need the calibration points)
     """
     
+    # Get image set:
+    image_set = get_image_set(object_type)
+
+
     # Fetch all images for the angle pair
-    img1_flat = load_calibration_image(image_set, angles[0], camera_position[0], "flat")
-    img2_flat = load_calibration_image(image_set, angles[1], camera_position[1], "flat")
-    img1_angled = load_calibration_image(image_set, angles[0], camera_position[0], "angled")
-    img2_angled = load_calibration_image(image_set, angles[1], camera_position[1], "angled")
-    img1_steep = load_calibration_image(image_set, angles[0], camera_position[0], "steep")
-    img2_steep = load_calibration_image(image_set, angles[1], camera_position[1], "steep")
+    img1_flat = load_calibration_image(object_type, angles[0], camera_position[0], "flat")
+    img2_flat = load_calibration_image(object_type, angles[1], camera_position[1], "flat")
+    img1_angled = load_calibration_image(object_type, angles[0], camera_position[0], "angled")
+    img2_angled = load_calibration_image(object_type, angles[1], camera_position[1], "angled")
+    img1_steep = load_calibration_image(object_type, angles[0], camera_position[0], "steep")
+    img2_steep = load_calibration_image(object_type, angles[1], camera_position[1], "steep")
     
     # Fetch points for all images
     points1_flat = get_calibration_points(img1_flat, "flat")
@@ -123,8 +173,8 @@ def get_foundamental_matrix(image_set, angles, camera_position, scale = 2.0, ret
 def epilines(img, points, lines, size = (12, 12)) :
     """ Draws a set of epilines and points on an image """
     # Generate figure
-    fig = figure(figsize=size)
-    imshow(img)
+    fig = pylab.figure(figsize=size)
+    pylab.imshow(img)
     
     # Get x values
     min_x = 0
@@ -133,40 +183,39 @@ def epilines(img, points, lines, size = (12, 12)) :
     max_y = img.shape[0]
     
     # Limit plot
-    xlim(0,max_x)
-    ylim(max_y-1,0)
+    pylab.xlim(0,max_x)
+    pylab.ylim(max_y-1,0)
     
     # plot lines
     for l, c in zip(lines, colors.get()) :
         # get line functions
         line_fun = lambda x : (-1 * l[0] * x - l[2]) / (float(l[1]))
         # plot line
-        plot([0, max_x], [line_fun(0), line_fun(max_x)], color=c, marker='_')
+        pylab.plot([0, max_x], [line_fun(0), line_fun(max_x)], color=c, marker='_')
     
     # plot points
     for p, c in zip(points, colors.get()) :
         # Plot feature match point
-        plot(p[0], p[1], color=c, marker='o')   
+        pylab.plot(p[0], p[1], color=c, marker='o')   
 
 
 
-def match_distances(matches, angles, match_object, image_set, check_threshold) :
+def match_distances(matches, angles, object_type, check_threshold) :
     """ Find the distance of matches as measured against two intersection epipolar lines:
         matches : List[(Pos,Pos)] (List of corresponding coordinates in two images)
         angles : (Int, Int) (two angles in degrees. Must be divisible by 5)
-        match_object : String (The type of 3d model we are looking at)
-        image_set : Int (The image set where this 3d model is found)
+        object_type : String (The type of 3d model we are looking at)
         check_threshold : Float (The threshold for a correct correspondence)
     """
     
     # Get features in B
-    path_B = get_turntable_path(image_set, match_object, angles[0], "Top")
+    path_B = get_turntable_path(object_type, angles[0], "Top")
     img_B = features.loadImage(path_B)
     
     # Find fundamental matrices
-    F_AC = get_foundamental_matrix(image_set, (angles[0], angles[1]), ("Bottom", "Bottom"), scale = 2.0)
-    F_AB = get_foundamental_matrix(image_set, (angles[0], angles[0]), ("Bottom", "Top"), scale = 2.0)
-    F_BC = get_foundamental_matrix(image_set, (angles[0], angles[1]), ("Top", "Bottom"), scale = 2.0)
+    F_AC = get_foundamental_matrix(object_type, (angles[0], angles[1]), ("Bottom", "Bottom"), scale = 2.0)
+    F_AB = get_foundamental_matrix(object_type, (angles[0], angles[0]), ("Bottom", "Top"), scale = 2.0)
+    F_BC = get_foundamental_matrix(object_type, (angles[0], angles[1]), ("Top", "Bottom"), scale = 2.0)
     
     # return distances
     return list(calc_match_distances(matches, img_B, F_AB, F_AC, F_BC, check_threshold))
@@ -212,3 +261,28 @@ def calc_match_distances(matches, img_B, F_AB, F_AC, F_BC, check_threshold) :
                 min_dist = max((numpy.min([dist(l_BC, p_C) for l_BC in lines_BC]), min_dist))
                 
         yield min_dist
+
+
+def get_image_set(object_type) :
+    sets = {
+        1 : ["Conch",  "FlowerLamp",        "Motorcycle",  "Rock", "Bannanas",  "Car",          "Desk",   "GrandfatherClock",  "Robot",       "TeddyBear", "Base",      "Car2",         "Dog",    "Horse", "Tricycle"],
+        2 : ["Calibration",  "Clock",  "EthernetHub",  "Hicama",  "Pepper"],
+        3 : ["Dremel",  "JackStand",  "Sander",  "SlinkyMonster",  "SprayCan"],
+        4 : ["FireExtinguisher",  "Frame",  "Hat", "StaplerRx"],
+        5 : ["Carton",  "Clamp",  "EggPlant",  "Lamp",  "Mouse",  "Oil"],
+        6 : ["Basket",  "Clipper",  "CupSticks",  "Filter",  "Mug",  "Shoe"],
+        7 : ["Pops", "Speaker", "BoxingGlove",  "CollectorCup", "Utilities"],
+        8 : ["Camera",  "DishSoap",  "Nesquik",  "PotatoChips",   "Sponge"],
+        9 : ["Camera2",     "Cloth",     "FloppyBox", "CementBase",  "Dinosaur",  "PaperBin"],
+        10 : ["Phone2",      "RollerBlade",  "Tripod",  "RiceCooker",  "Spoons",       "VolleyBall"],
+        11 : ["Gelsole",   "MouthGuard",  "Razor",       "Toothpaste", "Abroller",  "DVD",          "Keyboard",  "PS2"],
+        12 : ["Bush",  "DuckBank",  "Eggs",  "Frog",  "LightSaber"],
+        13 : ["Coffee",  "LavaBase",  "SwanBank",  "ToolBox",  "Vegetta"],
+        14 : ["BoxStuff",     "Standing", "BallSander",   "Rooster",     "StorageBin"],
+        15 : ["Globe",  "Pineapple"]
+    }
+    for key,value in sets.iteritems() :
+        if object_type in value :
+            return key
+
+    raise Exception("No object matching object_type of '%s'" % object_type)
